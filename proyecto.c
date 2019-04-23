a2c01f7071125fa4854033b609706ee96444ed6e
nicolascastrogarcia <47791639+nicoascastrogarcia@users.noreply.github.com>
2019-04-23 18:38:32 +0200

    añado codigo
    he añadido el menu de consumo y el calculo de maquinaria con errores

--- proyecto.c
+++ proyecto.c
@@ -5,7 +5,7 @@ int main()
 	char menu;
 	
 	do {
-		printf("Bienvenid@.\nPor favor, elige una opción:\n \
+		printf("Bienvenid@.\nPor favor, elige una opcion:\n \
 		Consumo (c)\n \
 		Gasto (g)\n \
 		Ahorro (a)\n \
@@ -14,7 +14,67 @@ int main()
 		
 		switch (menu) {
 			case 'c':
-				//Calcula el consumo de maquinaria,iluminación y otros servicios
+				//Calcula el consumo de maquinaria,iluminación
+				{
+					char cons;
+					do 
+					{
+					printf("Seleccione una opcion:\n");
+					printf("Consumo de maquinaria (m)\n");
+					printf("Consumo de iluminacion (i)\n");
+					printf("Salir (s)\n");
+					scanf("%c",&cons);
+					switch (cons)
+					{
+						case 'm':
+							{
+								int tip, i, *nmaq;
+								float *precio;
+								printf("Introduzca cuantos tipos de maquina ha comprado:\n");
+								scanf("%i",&tip);
+								precio = malloc(sizeof(double)*(tip+1));
+								nmaq = malloc(sizeof(int) * (tip+1));
+								//Comprueba si malloc ha funcionado
+								if (precio == NULL) {
+								printf("Error: memoria no disponible.\n");
+								exit(-1.0);
+								}
+								if (nmaq == NULL) {
+								printf("Error: memoria no disponible.\n");
+								exit(-1);
+								}
+								printf("Introduce el precio por maquina\n");
+								for (i=0;i<=tip;i++)
+								{
+									scanf("%f", precio[i+1]);
+									printf(" %.2f \t", *(precio + i));
+									free(precio);
+								}
+								printf("Introduce el numero de maquinas de cada tipo");
+								for (i=0;i<=tip;i++)
+								{
+									scanf("%i", nmaq[i+1]);
+									printf(" %i \t", *(nmaq + i));
+									free(nmaq);		
+								}
+								for (i=0;i<=tip;i++)
+								{
+								printf("El coste en total por tipo de maquina es:\n");
+								printf("%.2f",nmaq[i]*precio[i]);
+								}
+								break;
+
+
+							}
+							break;
+						case 'i':
+							;
+					}
+					
+				}
+				while (cons!= 's');
+					}
+
 				;
 				break;
 			case 'g':
