ae106231d364dc370568e909885ad29514e97693
Miguel Aspiroz de la Calle <miguel.aspiroz.delacalle@alumnos.upm.es>
2019-05-01 18:08:52 +0200

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -79,62 +79,144 @@ int main()
 				break;
 			case 'g':
 				{
-					char periodo;
-					do
-					{
-				//Muestra el gasto por dia, mes, o aÃ±o de agua, energÃ©tico y materias primas
-				
-				int duracion;
-				float agua, energia, matprim;
-				float aguah, energiah, matprimh;
-				float costea, costee, costemp;
-				printf("Elige si prefieres conocer el gasto de un dia (d), un mes (m) o un anno (a) o salir(s)\n");
-				scanf(" %c", &periodo);
-				switch (periodo) {
-					case 'd':
-						 {
+					//Muestra el gasto por dia, mes, o anno de agua, energetico y materias primas
+					char gasto, periodo;
+                    int duracion;
+                    float agua, energia, matprim;
+                    float aguah, energiah, matprimh; //gasto por unidad de tiempo
+                    float costea, costee, costemp;
+                    float consumo_agua(int duracion, float aguah); //funcion para calcular el consumo de agua
+	                do
+		            {
+		            printf("Elige si prefieres conocer el gasto de agua (a), energia (e) o materia prima (m). Pulsa (s) si quieres salir.\n");
+		            scanf(" %c", &gasto);
+		            switch (gasto) {
+			            case 'a':
+			            {
 						char infoagua;
-						duracion = 24; //horas en un dia
 						printf("El precio del agua en la Comunidad de Madrid es de 1,55 eur/m3.\n");
 						printf("Deseas conocer mas informacion sobre el consumo de agua? (s/n)\n");
 						scanf(" %c", &infoagua);
-						switch(infoagua)
-						{
-						case 's': {
-							printf("El consumo medio de agua en la industria nacional es de unos 15 m3/hora,\npero estos datos varian considerablemente segun el tipo.\nPor ejemplo, mientras que en la industria quimica la cifra llega a los 390,\nen la de la madera y el corcho es de apenas 1,3. (Fuente: INE)\n");
-							printf("Introduce los metros cubicos de agua consumidos por hora.\n");
-							scanf("%f", &aguah);
-							agua = aguah * 24;
-							costea = agua * 1.55;
-							printf("El consumo diario de agua es de %.2f metros cubicos.\nEl gasto diario en agua es de %.2f euros.\n", agua, costea);
-							}
-						break;
-						case 'n':
-						{
-						printf("Introduce los metros cubicos de agua consumidos por hora.\n");
-						scanf("%f", &aguah);
-						agua = aguah * 24;
-						costea = agua * 1.55;
-						printf("El consumo diario de agua es de %.2f metros cubicos.\nEl gasto diario en agua es de %.2f euros.\n", agua, costea);
-					}
-					break;
+						switch (infoagua) {
+							case 's':
+				    			{
+								printf("El consumo medio de agua en la industria nacional es de unos 15 m3/hora,\npero estos datos varian considerablemente segun el tipo.\nPor ejemplo, mientras que en la industria quimica la cifra llega a los 390,\nen la de la madera y el corcho es de apenas 1,3. (Fuente: INE)\n");
+								printf("Introduce los metros cubicos de agua consumidos por hora.\n");
+								scanf("%f", &aguah);
+								printf("Elige si prefieres calculcar el gasto diario (d), mensual (m) o anual (a).\n");
+								scanf(" %c", &periodo);
+								switch (periodo) {
+									case 'd':
+										duracion = 24; //horas en un dia
+										break;
+									case 'm':
+				            			duracion = 720; //horas en un mes
+				            			break;
+			            			case 'a':
+				            			duracion = 8760; //horas en un año
+				            			break;
+								}
+								consumo_agua(duracion,aguah);
+								}
+								break;
+							case 'n':
+				    			{
+								printf("Introduce los metros cubicos de agua consumidos por hora.\n");
+								scanf("%f", &aguah);
+								printf("Elige si prefieres calculcar el gasto diario (d), mensual (m) o anual (a).\n");
+								scanf(" %c", &periodo);
+								switch (periodo) {
+									case 'd':
+										duracion = 24; //horas en un dia
+										break;
+									case 'm':
+				            			duracion = 720; //horas en un mes
+				            			break;
+			            			case 'a':
+				            			duracion = 8760; //horas en un año
+				            			break;
+									}
+									consumo_agua(duracion,aguah);
+								}
+								break;
+			    			}
+		    			}
+			    			break;
+						case 'e':
+							//
+							break;
+						case 'm':
+						//
+							break;
+					}			
 				}
-						break;
-					case 'm':
-						duracion = 720; //horas en un mes
-						break;
-					case 'a':
-						duracion = 8760; //horas en un año
-						break;
-
-					}
-					
-				}
+<<<<<<< HEAD
+			while(periodo != 's');
+=======
 			}
 			while(periodo!='s');
 				break;
 			case 'a':
 				//Calcula el ahorro dependiendo de la eficiencia, las horas (valle, supervalle, punta) y energias renovables
+				//AHORRO
+int main()
+{
+int hora;
+float consumo,A,B,C,D,ef,contotal;
+char eficiencia, opcion;
+printf ("Escribe la hora y la clase energetica(A,B,C,D)\n");
+scanf ("%i %c", &hora, &eficiencia );
+
+switch (eficiencia)
+	{	case 'A':
+		ef=0.35;
+		break;
+		
+		case 'B':
+		ef=0.6;
+		break;
+		
+		case 'C':
+		ef=0.83;
+		break;
+		
+		case 'D':
+		ef=1;
+		break;
+		
+		default:
+		printf ("No existe esa clase energetica\n");
+		break;	
+		}	
+
+if (hora>13 && hora<24) 
+{	printf ("Es una hora punta\n"); 
+	consumo=0.162;
+	printf ("El consumo en euros por kilovatio hora es: %.3f kWh\n", consumo);  }	    
+
+else if ((hora>=0 && hora<=1) || (hora>7 && hora<13))
+{	printf("Es una hora valle\n");
+	consumo=0.093;
+ 	printf ("El consumo en euros por kilovatio hora es: %.3f kWh\n", consumo);  }
+
+if (hora>1 && hora<7)
+{	printf ("Es una hora supervalle\n"); 
+	consumo=0.071;
+ 	printf ("El consumo en euros por kilovatio hora es: %.3f kWh\n", consumo); }
+ 	
+contotal=ef*consumo;
+printf ("El consumo total, teniendo en cuenta la eficiencia es: %.4f", contotal);
+
+return 0;
+
+}
+
+float consumo(float dinh, float eficiencia)
+{
+	float consumo;
+	consumo=dinh*eficiencia;
+	return consumo;
+}
 				;
 				break;
 			case 's':
@@ -143,10 +225,18 @@ int main()
 			default:
 				printf("Elige una opcion valida, por favor.\n");
 				break;
+>>>>>>> 926b9499d0fcaa0eaf9a1fe0188825c3cd9471ec
 		}
 	}
 }
 	while (menu != 's');
 	
   return 0;
+}
+
+float consumo_agua(int duracion, float aguah) {
+	float agua, costea;
+	agua = aguah * duracion;
+	costea = agua * 1.55;
+	printf("El consumo de agua es de %.2f metros cubicos.\nEl gasto en agua es de %.2f euros.\n\n", agua, costea);
 }
